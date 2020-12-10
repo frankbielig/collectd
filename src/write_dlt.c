b99ba48ed4569e4c64a6c9b62be9e133a972fc35
Frank Bielig <frank.bielig@daimler.com>
2020-12-10 15:38:07 +0100

    write_dlt: support log level support

--- src/write_dlt.c
+++ src/write_dlt.c
@@ -1,5 +1,5 @@
 /**
- * Copyright has to be defined
+ * SPDX-FileCopyrightText: 2020 Daimler AG
  **/
 
 #include "collectd.h"
@@ -12,19 +12,133 @@
 
 #include "dlt/dlt.h"
 
-#define WL_BUF_SIZE 16384
+#if HAVE_REGEX_H
+#include <regex.h>
+#endif
 
+#define WL_BUF_SIZE 16384
 #define WL_FORMAT_GRAPHITE 1
 #define WL_FORMAT_JSON 2
-
 
 DLT_DECLARE_CONTEXT(jsonContext);
 DLT_DECLARE_CONTEXT(graphiteContext);
     
-/* Plugin:WriteLog has to also operate without a config, so use a global. */
+/* -------------------------------------------------------------------------- */
+/* dlt global variables */
+
 int wdlt_format = WL_FORMAT_GRAPHITE;
 char wdlt_appid[] = "CLTD";
-const char* wdlt_name = "write_dlt";
+const char* wdlt_name = "write_dlt plugin";
+
+/* -------------------------------------------------------------------------- */
+/* matching list */
+typedef struct match_entry_s {
+#if HAVE_REGEX_H
+  regex_t *re;
+#endif
+  DltLogLevelType dlt_level;
+  struct match_entry_s* _next;
+} match_entry_t;
+
+static match_entry_t* match_list_begin = NULL;
+static match_entry_t* match_list_end = NULL;
+
+/* -------------------------------------------------------------------------- */
+static void wdlt_match_list_add(const char* regexp, const char* level) {
+    int status;
+
+    match_entry_t* match_entry = malloc(sizeof(match_entry_t));
+    if (match_entry == NULL) {
+      ERROR("%s: match_list_add: malloc failed.", wdlt_name);
+      return ;
+    }
+
+#if HAVE_REGEX_H
+  if (regexp != NULL) {
+    match_entry->re = calloc(1, sizeof(*match_entry->re));
+    if (match_entry->re == NULL) {
+      ERROR("%s: match_list_add: calloc failed.", wdlt_name);
+      sfree(match_entry);
+      return;
+    }
+
+    status = regcomp(match_entry->re, regexp, REG_EXTENDED | REG_NOSUB);
+    if (status != 0) {
+      DEBUG("%s: compiling the regular expression \"%s\" failed.",
+            mdlt_name, regexp);
+      sfree(match_entry->re);
+      sfree(match_entry);
+      return;
+    }
+  }
+#else
+  if (regexp != NULL) {
+    ERROR("%s: ps_list_register: "
+          "Regular expression \"%s\" found in config "
+          "file, but support for regular expressions "
+          "has been disabled at compile time.",
+          wdlt_name, regexp);
+    sfree(match_entry);
+    return;
+  }
+#endif
+
+    match_entry->dlt_level = DLT_LOG_INFO;
+    if (level != NULL) {
+      if(strcasecmp(level, "DEFAULT") == 0) {
+        match_entry->dlt_level = DLT_LOG_DEFAULT;
+      } else if(strcasecmp(level, "OFF") == 0) {
+        match_entry->dlt_level = DLT_LOG_OFF;
+      } else if(strcasecmp(level, "FATAL") == 0) {
+        match_entry->dlt_level = DLT_LOG_FATAL;
+      } else if(strcasecmp(level, "ERROR") == 0) {
+        match_entry->dlt_level = DLT_LOG_ERROR;
+      } else if(strcasecmp(level, "WARN") == 0) {
+        match_entry->dlt_level = DLT_LOG_WARN;
+      } else if(strcasecmp(level, "INFO") == 0) {
+        match_entry->dlt_level = DLT_LOG_INFO;
+      } else if(strcasecmp(level, "DEBUG") == 0) {
+        match_entry->dlt_level = DLT_LOG_DEBUG;
+      } else if(strcasecmp(level, "VERBOSE") == 0) {
+        match_entry->dlt_level = DLT_LOG_VERBOSE;
+      }
+    }
+      
+    if (match_list_begin == NULL) {
+      match_list_begin = match_entry;
+      match_list_end = match_entry;
+    } else {
+      match_list_end->_next = match_entry;      
+      match_list_end = match_entry;      
+    }
+
+}
+
+/* -------------------------------------------------------------------------- */
+static void wdlt_match_list_clear() {
+  while (match_list_begin != NULL) {
+    match_entry_t* match_entry_to_delete = match_list_begin;
+    match_list_begin = match_entry_to_delete->_next;
+  }
+  match_list_end = NULL;
+}
+
+/* -------------------------------------------------------------------------- */
+static DltLogLevelType wdlt_match_list_get_dlt_level(const char* message) {
+#if HAVE_REGEX_H
+  match_entry_t* me;
+  for (me = match_list_begin; me != NULL; me = me->_next) {
+    if (me->re == NULL) {
+      continue;
+    }
+    if (regexec(me->re, message, 0, NULL, 0) == 0) {
+      return me->dlt_level;
+    }
+  }
+#endif
+  return DLT_LOG_INFO;
+}
+
 
 /* -------------------------------------------------------------------------- */
 static int wdlt_write_graphite(const data_set_t *ds, const value_list_t *vl) {
@@ -41,10 +155,11 @@ static int wdlt_write_graphite(const data_set_t *ds, const value_list_t *vl) {
   if (status != 0) /* error message has been printed already. */
     return status;
 
-  DLT_LOG(graphiteContext, DLT_LOG_INFO, DLT_STRING(buffer));
+  DltLogLevelType dlt_level = wdlt_match_list_get_dlt_level(buffer);
+  DLT_LOG(graphiteContext, dlt_level, DLT_STRING(buffer));
 
   return 0;
-} /* int wdlt_write_graphite */
+}
 
 
 /* -------------------------------------------------------------------------- */
@@ -63,10 +178,11 @@ static int wdlt_write_json(const data_set_t *ds, const value_list_t *vl) {
                          /* store rates = */ 0);
   format_json_finalize(buffer, &bfill, &bfree);
 
-  DLT_LOG(jsonContext, DLT_LOG_INFO, DLT_STRING(buffer));
+  DltLogLevelType dlt_level = wdlt_match_list_get_dlt_level(buffer);
+  DLT_LOG(jsonContext, dlt_level, DLT_STRING(buffer));
 
   return 0;
-} /* int wdlt_write_json */
+}
 
 
 /* -------------------------------------------------------------------------- */
@@ -85,7 +201,7 @@ static int wdlt_write(const data_set_t *ds, const value_list_t *vl,
 
 
 /* -------------------------------------------------------------------------- */
-static int wg_config_dlt(oconfig_item_t *ci)  /* {{{ */
+static int wg_config_dlt(oconfig_item_t *ci) 
 {
   for (int i = 0; i < ci->children_num; i++) {
     oconfig_item_t *child = ci->children + i;
@@ -93,6 +209,16 @@ static int wg_config_dlt(oconfig_item_t *ci)  /* {{{ */
     if (strcasecmp("AppID", child->key) == 0) {
       bzero(wdlt_appid, 4);
       cf_util_get_string_buffer(child, wdlt_appid, sizeof(wdlt_appid));
+    } else if (strcasecmp("MatchLevel", child->key) == 0) {
+      if ((child->values_num != 2) ||
+          (OCONFIG_TYPE_STRING != child->values[0].type) ||
+          (OCONFIG_TYPE_STRING != child->values[1].type)) {
+        ERROR("%s: `ProcessMatch' needs exactly two string arguments (got %i).",
+              wdlt_name, child->values_num);
+        continue;
+      }
+      wdlt_match_list_add(child->values[0].value.string, 
+                          child->values[1].value.string);
     } else {
       ERROR("%s: Invalid configuration option in <DLT>: `%s'.",
             wdlt_name, child->key);
@@ -100,11 +226,11 @@ static int wg_config_dlt(oconfig_item_t *ci)  /* {{{ */
     }
   }
   return 0;
-} /* }}} int wdlt_config_dlt */  
+}
 
 
 /* -------------------------------------------------------------------------- */
-static int wdlt_config(oconfig_item_t *ci) /* {{{ */
+static int wdlt_config(oconfig_item_t *ci)
 {
   bool format_seen = false;
   
@@ -147,11 +273,11 @@ static int wdlt_config(oconfig_item_t *ci) /* {{{ */
   }
 
   return 0;
-} /* }}} int wdlt_config */
+}
 
 
 /* -------------------------------------------------------------------------- */
-static int wdlt_init() /* {{{ */
+static int wdlt_init()
 {
   INFO("write_dlt: register app with '%s'.", wdlt_appid);
   DLT_REGISTER_APP(wdlt_appid, "Diagnostic Log and Trace");
@@ -160,25 +286,26 @@ static int wdlt_init() /* {{{ */
   DLT_REGISTER_CONTEXT(graphiteContext, "GRPH", "use graphite format");
 
   return 0;
-} /* }}} int wdlt_init */
+}
 
 
 /* -------------------------------------------------------------------------- */
-static int wdlt_shutdown() /* {{{ */
+static int wdlt_shutdown()
 {
+  wdlt_match_list_clear();
+
   DLT_UNREGISTER_CONTEXT(jsonContext);
   DLT_UNREGISTER_CONTEXT(graphiteContext);
 
   INFO("write_dlt: unregister app with '%s'.", wdlt_appid);
   DLT_UNREGISTER_APP();
   return 0;
-} /* }}} int wdlt_shutdown */
+} 
 
 
 /* -------------------------------------------------------------------------- */
 void module_register(void) {
   plugin_register_complex_config("write_dlt", wdlt_config);
-  /* If config is supplied, the global wdlt_format will be set. */
   plugin_register_write("write_dlt", wdlt_write, NULL);
 
   plugin_register_init ("my_plugin", wdlt_init);
