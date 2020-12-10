3b5060eb1ff9a8a378015f51becc2156ae051fd3
Frank Bielig <frank.bielig@daimler.com>
2020-12-10 22:44:30 +0100

    rebuild.sh: enable full debugging

--- rebuild.sh
+++ rebuild.sh
@@ -7,12 +7,13 @@ INSTALL_DIR="$BASE_DIR/install"
 $BASE_DIR/clean.sh
 $BASE_DIR/build.sh
 
-
+CFLAGS="-g -O0" \
 ./configure \
   --prefix="$INSTALL_DIR" \
   --with-perl-bindings="INSTALL_BASE=$INSTALL_DIR" \
   --with-libdlt="$HOME/work/github/GENIVI/dlt-daemon/install" \
-  --disable-perl
+  --disable-perl \
+  --enable-debug
   
   
 make -j 6
