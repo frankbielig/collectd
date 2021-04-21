919b0c00bfa75d6da013c300710d627fc32b3c08
Frank Bielig <frank.bielig@daimler.com>
2021-04-21 16:04:56 +0200

    client library: make building of libcollectdclient optional

--- rebuild.sh
+++ rebuild.sh
@@ -4,6 +4,7 @@ SELF=$(cd $(dirname "$0"); pwd)/$(basename "$0")
 BASE_DIR=$(dirname "$SELF")
 INSTALL_DIR="$BASE_DIR/install"
 
+rm -rf $INSTALL_DIR
 $BASE_DIR/clean.sh
 $BASE_DIR/build.sh
 
@@ -13,9 +14,9 @@ CFLAGS="-g -O0" \
   --with-perl-bindings="INSTALL_BASE=$INSTALL_DIR" \
   --with-libdlt="$HOME/work/github/GENIVI/dlt-daemon/install" \
   --disable-perl \
+  --disable-client-library \
   --enable-debug
-  
-  
+
 make -j 6
 make check
 make install
